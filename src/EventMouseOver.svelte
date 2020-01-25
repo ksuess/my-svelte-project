e8daeb59036943ca41da5a221b3b25230064d610
Katja Suess <k.suess@rohberg.ch>
2020-01-25 14:26:08 +0100

    event

---
+++ src/EventMouseOver.svelte
@@ -0,0 +1,19 @@
+<script>
+  let m = { x: 0, y: 0 };
+
+  function handleMousemove(event) {
+    m.x = event.clientX;
+    m.y = event.clientY;
+  }
+</script>
+
+<style>
+  div { width: 100%; height: 100%; min-height: 100px;
+    background-color: #eee;
+    margin-bottom: 1rem;
+  }
+</style>
+
+<div on:mousemove={handleMousemove}>
+  The mouse position is {m.x} x {m.y}
+</div>
